ac9be207d96c333c061834e3777d8ac7133db19c
EnzoRss <enzo.rossi@ynov.com>
2023-09-11 17:00:32 +0200

    Start json

--- src/API/app.js
+++ src/API/app.js
@@ -1,10 +1,12 @@
 const express= require('express');
 const app = express();
 const port= 80;
+const routegame = require('route.js');
 
+app.use(cors({
+    origin: '*'
+}))
 
-app.get('/',(req,res)=>{
-    res.send("hello")
-});
+app.use(routegame);
 
 app.listen(port,()=>console.log(`Server listening on port ${port}`));
